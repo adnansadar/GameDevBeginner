20f599737ac17ab87395608e92d0d91dd72509f2
adnansadar <adnansadar11@gmail.com>
2020-01-11 08:59:54 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();//package used for opening the window
+        frame.pack();
+        frame.setSize(640,480);//setting the size of the window
+        frame.setResizable(false);//user is not allowed to change the window size
+        frame.setLocationRelativeTo(null);//used for making the window appear in the center
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//closes the window and terminates the program when x button is clicked
+        frame.setVisible(true);//window is made visible
+    }
+}
